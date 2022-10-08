cb16b9d8254e75eca44e5c5f8c6cbe8dd380f94e
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-10-08 14:04:25 +0900

    Add styled, mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { theme } from "@/styles/theme";
+import { CssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
