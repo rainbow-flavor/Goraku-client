4edb791c3b6bd295889689b00b1040222e315081
Hank <seolin9340@gmail.com>
2023-03-04 17:17:02 +0900

    style(SearchInput): 애니메이션 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,35 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 import styles from "@/styles/Home.module.css";
 
 const HomePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => setIsOpen((prev) => !prev);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Goraku</h1>
-      <input className={styles.searchInput} type="text" />
+
+      <div
+        className={styles.searchInputBox}
+        onFocus={() => setIsOpen(true)}
+        onBlur={() => setIsOpen(false)}
+      >
+        <input
+          className={clsx(
+            styles.searchInput,
+            isOpen && styles.searchInputActive
+          )}
+          type="text"
+        />
+        {isOpen && (
+          <ul className={styles.searchList}>
+            <li>111</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
