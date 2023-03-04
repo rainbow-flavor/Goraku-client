3bc465f7bbcba301c08eb0bd85685f28112d6929
Hank <seolin9340@gmail.com>
2023-03-04 16:26:48 +0900

    feat: Home title

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
-export default function Document() {
+const Document = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="kr">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
+};
+
+export default Document;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import styles from "@/styles/Home.module.css";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Goraku</h1>
+      <input className={styles.searchInput} type="text" />
+    </div>
+  );
 };
 
 export default HomePage;
