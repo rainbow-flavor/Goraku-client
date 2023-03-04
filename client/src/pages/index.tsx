671197fed2a634d08936a6fd593c81c7a2a1c94e
Hank <seolin9340@gmail.com>
2023-03-04 18:27:08 +0900

    fix: 인풋 이벤트 문제 해결

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,32 +1,59 @@
-import clsx from "clsx";
-import { useState } from "react";
+import { clsx } from "clsx";
+import {
+  FocusEventHandler,
+  LegacyRef,
+  MutableRefObject,
+  useRef,
+  useState,
+} from "react";
 
+import useInput from "@/hooks/use-input";
+import useOutside from "@/hooks/use-outside";
+import { searchList } from "@/mock/home";
 import styles from "@/styles/Home.module.css";
 
 const HomePage = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const inputFieldRef = useRef<HTMLDivElement>(null);
+  const { updateValue, ...inputProps } = useInput();
 
-  const onClick = () => setIsOpen((prev) => !prev);
+  const closeSearchField = () => setIsOpen(false);
+
+  const onSelectItem = (arg: string) => {
+    updateValue(arg);
+    closeSearchField();
+  };
+
+  useOutside(inputFieldRef, closeSearchField);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Goraku</h1>
 
-      <div
-        className={styles.searchInputBox}
-        onFocus={() => setIsOpen(true)}
-        onBlur={() => setIsOpen(false)}
-      >
+      <div className={styles.searchInputBox} ref={inputFieldRef}>
         <input
+          onFocus={() => setIsOpen(true)}
           className={clsx(
             styles.searchInput,
             isOpen && styles.searchInputActive
           )}
           type="text"
+          {...inputProps}
         />
+
         {isOpen && (
           <ul className={styles.searchList}>
-            <li>111</li>
+            {searchList.map((tag, index) => {
+              return (
+                <li
+                  key={index}
+                  className={styles.searchItem}
+                  onClick={() => onSelectItem(tag)}
+                >
+                  {tag}
+                </li>
+              );
+            })}
           </ul>
         )}
       </div>
