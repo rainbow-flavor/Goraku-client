0104ef0d269f2a290f46f21840be5efd2bff8996
irostub <irostub@gmail.com>
2023-05-14 04:24:16 +0900

    fix monitoring 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,7 @@
+// @ts-ignore
+import WhatapAgent from 'whatap';
+WhatapAgent.NodeAgent;
+
 import "@/styles/globals.css";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -8,10 +12,6 @@ import React, { ReactNode, useEffect } from "react";
 import { RecoilRoot, useRecoilSnapshot } from "recoil";
 import { useGeolocationAtom } from "@/atoms/geolocation-atom";
 import Modal from "@/components/common/modal/Modal";
-
-// @ts-ignore
-import WhatapAgent from 'whatap';
-WhatapAgent.NodeAgent;
 
 const DebugObserver = () => {
   const snapshot = useRecoilSnapshot() as any;
