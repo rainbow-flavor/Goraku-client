45328402357747bf61b3379805f37e5df2e9e2dc
irostub <irostub@gmail.com>
2023-05-14 06:16:31 +0900

    Revert "whatap 적용"
    This reverts commit cd398dfbc8bd1075b74ddec67c7fd982c8783c0b.

--- next.config.js
+++ next.config.js
@@ -1,50 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   webpack5: true,
-//   webpack: (config,{ isServer,webpack },) => {
-//     if(isServer === false){
-//       config.plugins.push(new webpack.IgnorePlugin({resourceRegExp: /^whatap/, contextRegExp: /.*$/}));
-//     }
-//     return config;
-//   },
-//
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/v1/:path*",
-//         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-//       },
-//     ];
-//   },
-// };
-
-module.exports = async(phase, defaultConfig)=>{
-  const nextConfig = {
-    reactStrictMode: true,
-    webpack5: true,
-    webpack: (config,{ isServer,webpack },) => {
-      if(isServer === false){
-        config.plugins.push(new webpack.IgnorePlugin({resourceRegExp: /^whatap/, contextRegExp: /.*$/}));
-      }
-      return config;
-    },
-
-    async rewrites() {
-      return [
-        {
-          source: "/api/v1/:path*",
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-        },
-      ];
-    },
-  };
-
-  // @ts-ignore
-  import WhatapAgent from 'whatap';
-  WhatapAgent.NodeAgent;
-
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config,{ isServer,webpack },) => {
+    if(isServer === false){
+      config.plugins.push(new webpack.IgnorePlugin({resourceRegExp: /^whatap/, contextRegExp: /.*$/}));
+    }
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
 
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+// @ts-ignore
+import WhatapAgent from 'whatap';
 import "@/styles/globals.css";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -9,6 +11,7 @@ import { RecoilRoot, useRecoilSnapshot } from "recoil";
 import { useGeolocationAtom } from "@/atoms/geolocation-atom";
 import Modal from "@/components/common/modal/Modal";
 
+WhatapAgent.NodeAgent;
 const DebugObserver = () => {
   const snapshot = useRecoilSnapshot() as any;
   useEffect(() => {
