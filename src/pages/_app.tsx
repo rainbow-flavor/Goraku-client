8f2d1bf659504b767da6c0ed3d0880c7e6685de3
Hank <seolin9340@gmail.com>
2023-05-28 21:15:16 +0900

    fix: favicon load error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -73,7 +73,31 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <script src="https://repo.whatap-browser-agent.io/rum/prod/v1/whatap-browser-agent.js" />
       </Head>
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,26 +5,6 @@ const Document = () => {
   return (
     <Html lang="kr">
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
